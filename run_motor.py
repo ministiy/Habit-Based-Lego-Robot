05fdd6a0ff2e1993dfef46e261307855f33bae42
ripperonipepperoni <darius.au96@gmail.com>
2017-12-05 14:19:35 +1300

    ev3dev tutorial first commit

--- run_motor.py
+++ run_motor.py
@@ -4,6 +4,7 @@ import time
 #Spin the left motor for 3 seconds
 m = LargeMotor('outB')
 n = LargeMotor('outC')
+
 m.run_timed(time_sp=500, speed_sp=500)
-n.run_timed(time_sp=500, speed_sp=-500)
+#n.run_timed(time_sp=500, speed_sp=500)
 
