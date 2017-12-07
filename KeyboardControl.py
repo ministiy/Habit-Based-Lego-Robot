b0af533dabe11d0e2b8769c25d12c2d184581778
ripperonipepperoni <darius.au96@gmail.com>
2017-12-07 13:20:25 +1300

    keyboard control with data recorded

--- KeyboardControl.py
+++ KeyboardControl.py
@@ -6,6 +6,14 @@
 
 import termios, tty, sys
 from ev3dev.ev3 import *
+import csv
+
+MAX_SENSOR = 100.0 # percent
+MAX_MOTOR = 1000.0
+
+BIAS = 0.05
+SENSOR_GAIN = 1.0
+OUTPUT_GAIN = 1.0
 
 # attach large motors to ports B and C
 motor_left = LargeMotor('outB')
@@ -14,6 +22,17 @@ motor_right = LargeMotor('outC')
 motor_left.reset()
 motor_right.reset()
 
+ls = ColorSensor('in2')
+#assert ls.connected, "Left sensor not connected to port 2"
+rs = ColorSensor('in3')
+#assert rs.connected, "Right sensor not connected to port 3"
+ls.mode='COL-AMBIENT'
+rs.mode='COL-AMBIENT'
+
+lu = UltrasonicSensor('in1')
+lu.mode='US-DIST-CM'
+ru = UltrasonicSensor('in4')
+ru.mode='US-DIST-CM'
 
 # ==============================================
 
@@ -69,6 +88,11 @@ def stop():
 
 # ==============================================
 
+header = ['left sensor','right sensor' , 'left ultraviolet sensor' , 'right ultraviolet sensor' , 'left motor', 'right motor']
+with open('output.csv', 'w', newline="") as output_file:
+        wr = csv.writer(output_file,delimiter = ',' , quoting = csv.QUOTE_ALL)
+        wr.writerow(header)
+
 print("Program started")
 
 while True:
@@ -88,3 +112,57 @@ while True:
         stop()
     if k == 'q':
         exit()
+
+        ## normalized to lie between 0 and 1 (1 close, 0 far)
+    lsv = SENSOR_GAIN * float(ls.value()) / MAX_SENSOR
+    rsv = SENSOR_GAIN * float(rs.value()) / MAX_SENSOR
+
+    luv = 1.0 - max(0.0, min(1.0, float(lu.value()) / 200.0))
+    ruv = 1.0 - max(0.0, min(1.0, float(ru.value()) / 200.0))
+
+    Leds.set(Leds.LEFT, brightness_pct=lsv)
+    Leds.set(Leds.RIGHT, brightness_pct=rsv)
+
+    # if max(lsv,rsv) < 0.1 :
+    #     SENSOR_GAIN *=1.05
+    #     print('SENSOR_GAIN increased to : %f' %(SENSOR_GAIN))
+    # elif min(lsv,rsv) > 0.5 :
+    #     SENSOR_GAIN *=0.95
+    #     print('SENSOR_GAIN decreased to : %f' %(SENSOR_GAIN))
+
+    # ## LOVE + AGGR
+    # lmv = BIAS + (rsv-0.2*lsv)
+    # rmv = BIAS + (lsv-0.2*rsv)
+
+    ## AGGR
+    lmv = BIAS + rsv - 0.0 * lsv - (ruv * 2.)
+    rmv = BIAS + lsv - 0.0 * rsv - (luv * 2.)
+
+    lmv *= OUTPUT_GAIN
+    rmv *= OUTPUT_GAIN
+
+    if max(lmv, rmv) > MAX_MOTOR:
+        lmv -= max_mv - MAX_MOTOR
+        rmv -= max_mv - MAX_MOTOR
+        # OUTPUT_GAIN *= 0.95
+        # print('OUTPUT_GAIN decreased to : %f' %(OUTPUT_GAIN))
+
+    # if min(lmv,rmv) < 0.05 :
+    #     OUTPUT_GAIN *= 1.05
+    #     print('OUTPUT_GAIN increased to : %f' %(OUTPUT_GAIN))
+
+
+    lmv = int(max(-1000, min(1000, MAX_MOTOR * lmv)))
+    rmv = int(max(-1000, min(1000, MAX_MOTOR * rmv)))
+
+
+    # writing to a csv file called output.csv to store sensory-motor data where
+    #   lsv = left colour sensor value
+    #   rsv = right colour sensor value
+    #   luv = left ultraviolet sensor value
+    #   ruv = right ultraviolet sensor value
+    #   lmv = left motor value
+    #   rmv = right motor value
+    with open('output.csv', 'a', newline="") as output_file:
+        wr = csv.writer(output_file, delimiter=',', quoting=csv.QUOTE_ALL)
+        wr.writerow([lsv, rsv, luv, ruv, lmv, rmv])
