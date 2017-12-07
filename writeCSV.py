d9496a447b554888dd27168b80e5abe53f0b6751
ripperonipepperoni <darius.au96@gmail.com>
2017-12-07 13:45:31 +1300

    Fixed more indentation errors between PyCharm and Notepad++

--- writeCSV.py
+++ writeCSV.py
@@ -1,14 +1,15 @@
 import csv
 
-class WriteCSV:
-	def writeHeader():
-		header = ['left sensor','right sensor' , 'left ultraviolet sensor' , 'right ultraviolet sensor' , 'left motor', 'right motor']
-		with open('output.csv', 'w', newline="") as output_file:
-				wr = csv.writer(output_file,delimiter = ',' , quoting = csv.QUOTE_ALL)
-				wr.writerow(header)
 
-				
-	def writeData():
+class WriteCSV:
+    def writeHeader(self):
+        header = ['left sensor', 'right sensor', 'left ultraviolet sensor', 'right ultraviolet sensor', 'left motor',
+                  'right motor']
+        with open('output.csv', 'w', newline="") as output_file:
+            wr = csv.writer(output_file, delimiter=',', quoting=csv.QUOTE_ALL)
+            wr.writerow(header)
+
+        def writeData(self):
 		# writing to a csv file called output.csv to store sensory-motor data where
 			#   lsv = left colour sensor value
 			#   rsv = right colour sensor value
