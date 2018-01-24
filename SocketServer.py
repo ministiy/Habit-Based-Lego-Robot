3658d9ab2a45b630765e223c06db50996c45cf0e
teddy <teddysaputra@hotmail.com>
2018-01-25 11:37:08 +1300

    Add a safeguard to prevent failure

--- SocketServer.py
+++ SocketServer.py
@@ -19,14 +19,17 @@ class CSVBackgroundThread (threading.Thread):
    def run(self):
         # Printing and writing to CSV file
         while True:
-            data = conn.recv(4096)
-            listOfValues = pickle.loads(data)
-            for i in range(Constant.PACKAGE_SIZE):
-                print('ls:%0.3f rs:%0.3f lu:%0.3f ru:%0.3f lm:%0.3f rm:%0.3f' % (listOfValues[0], listOfValues[1], listOfValues[2], listOfValues[3], listOfValues[4], listOfValues[5]))
-                self.writer.writeData(listOfValues[:6])
-                listOfValues = listOfValues[6:]
-                #   print(listOfValues)
-
+            try:
+                data = conn.recv(4096)
+                listOfValues = pickle.loads(data)
+                for i in range(Constant.PACKAGE_SIZE):
+                    print('ls:%0.3f rs:%0.3f lu:%0.3f ru:%0.3f lm:%0.3f rm:%0.3f' % (listOfValues[0], listOfValues[1], listOfValues[2], listOfValues[3], listOfValues[4], listOfValues[5]))
+                    self.writer.writeData(listOfValues[:6])
+                    listOfValues = listOfValues[6:]
+                    #   print(listOfValues)
+            except:
+                continue
+                
 # ==== CSV FUNCTIONS ==== #
 # ==============================================
 
